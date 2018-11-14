4e00c9857094593c9941ef6be0d9476b347faf93
AntonVoronezh <pstut@mail.ru>
2018-11-14 11:23:15 +0300

    2 #4

--- dist/js/task2.js
+++ dist/js/task2.js
@@ -19,3 +19,6 @@ createDiv2.classList.add('inner');
 
 // 3 При помощи DOM API, поместите в элемент текст: 'Этот элемент тоже создан при помощи DOM API'
 createDiv2.textContent = 'Этот элемент тоже создан при помощи DOM API';
+
+// 4 Поместите созданный элемент внутрь элемента, который был создан в предыдущем задании
+createDiv.appendChild(createDiv2);

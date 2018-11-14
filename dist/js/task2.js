44f7933b7c8e8ac487b847b01ca379427917173f
AntonVoronezh <pstut@mail.ru>
2018-11-14 11:21:13 +0300

    1 #2

--- dist/js/task2.js
+++ dist/js/task2.js
@@ -2,3 +2,6 @@
 // 1 При помощи DOM API создайте DOM-элемент с тегом div
 const createDiv = document.createElement('div');
 
+// 2 Добавьте созданный элемент в DOM-дерево
+const getBody = document.querySelector('body');
+getBody.appendChild(createDiv);

f1e61aebcb51700093294710b69f001948c1fcf9
AntonVoronezh <pstut@mail.ru>
2018-11-14 11:21:36 +0300

    1 #3'

--- dist/js/task2.js
+++ dist/js/task2.js
@@ -5,3 +5,6 @@ const createDiv = document.createElement('div');
 // 2 Добавьте созданный элемент в DOM-дерево
 const getBody = document.querySelector('body');
 getBody.appendChild(createDiv);
+
+// 3 При помощи DOM API, поместите в элемент текст: 'Этот элемент создан при помощи DOM API'
+createDiv.textContent = 'Этот элемент создан при помощи DOM API';

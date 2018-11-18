0d4f9460f69885bf2bfa1d69bc829ec17ef94b07
AntonVoronezh <pstut@mail.ru>
2018-11-18 14:08:44 +0300

    add bundle.j

---
+++ dist/js/bundle.js
@@ -0,0 +1,22 @@
+var closeLink = document.querySelector(".close__link");
+console.log(closeLink);
+
+var mobileMenu = document.querySelector(".mobile-menu");
+console.log(mobileMenu);
+
+var openLink = document.querySelector(".menu-tablets");
+console.log(openLink);
+
+openLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    mobileMenu.classList.add("mobile-menu-open");
+    // console.log('open');
+});
+
+closeLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    mobileMenu.classList.remove("mobile-menu-open");
+    // console.log('close');
+});
+
+
